7c6f03847dd5bdb3d7ba9cf7a6e071df1f540539
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-05-15 03:21:50 +0400

    PEP8

--- bars.py
+++ bars.py
@@ -1,39 +1,61 @@
 import requests
 import json
-url="https://devman.org/media/filer_public/95/74/957441dc-78df-4c99-83b2-e93dfd13c2fa/bars.json"
-req=requests.get(url)
-bars=req.json()
-d=bars["features"] #поиск по ключу features
-a=[] #создание списка для поиска max и min
-for i in d:
-    seats=i["properties"]["Attributes"]["SeatsCount"] #перебираем количество мест
-    a.append(seats) #добавляем в список
-max_size=max(a) 
-min_size=min(a)
-for i in d:
-    seats=i["properties"]["Attributes"]["SeatsCount"] #перебираем количество мест
-    if max_size==seats:
+
+url = "https://devman.org/media/filer_public/95/74/\
+957441dc-78df-4c99-83b2-e93dfd13c2fa/bars.json"
+response = requests.get(url)
+bars = response.json()
+# поиск по ключу features
+dict_bars = bars["features"]
+# создание списка для поиска max и min
+arr_0 = []
+
+for i in dict_bars:
+    seats = i["properties"]["Attributes"]["SeatsCount"]
+# добавляем в список
+    arr_0.append(seats)
+
+max_size = max(arr_0)
+min_size = min(arr_0)
+for i in dict_bars:
+    seats = i["properties"]["Attributes"]["SeatsCount"]
+    if max_size == seats:
         print("Самый большой бар: ")
         print(i["properties"]["Attributes"]["Name"])
-    elif min_size==seats:
+    elif min_size == seats:
         print("Самый маленький бар: ")
         print(i["properties"]["Attributes"]["Name"])
 
 print("Вместимость самого большого бара {}".format(max_size))
 print("Вместимость самого маленького бара {}".format(min_size))
-#поиск по координатам
-x=float(input("Введите координаты\n")) #вводим координаты x
-y=float(input("Введите координаты\n")) #вводим координаты y
-arr_1=[] 
-for i in d:
-    coordinates=i["geometry"]["coordinates"]
+
+# поиск по координатам
+x_coord = float(input("Введите координаты \n"))
+y_coord = float(input("Введите координаты \n"))
+
+arr_1 = []
+for i in dict_bars:
+    coordinates = i["geometry"]["coordinates"]
     arr_1.append(coordinates)
-arr_2=[]
+arr_2 = []
 for i in arr_1:
-    diff_x=x-i[0] # находим разницу по координатам х
-    diff_y=y-i[1] # находим разницу по координатам y
-    sum_of_diffs=abs(diff_x)+abs(diff_y) # находим сумму разности x и y
+
+    diff_x = x_coord-i[0]
+
+# находим разницу по координатам
+
+    diff_y = y_coord-i[1]
+
+# находим сумму разности x и y
+    sum_of_diffs = abs(diff_x) + abs(diff_y)
+
     arr_2.append(sum_of_diffs)
-    min_coord=min(arr_2) # находим минимальное отклонение от координат
-min_index=arr_2.index(min_coord) #находим индекс 
-print("Ближайший к вам бар: ",bars["features"][min_index]["properties"]["Attributes"]["Name"])
+
+# находим минимальное отклонение от координат
+    min_coord = min(arr_2)
+
+# находим индекс
+min_index = arr_2.index(min_coord)
+
+print("Ближайший к вам бар: ",
+      bars["features"][min_index]["properties"]["Attributes"]["Name"])
