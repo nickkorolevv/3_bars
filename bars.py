462bdea100135381290e8efe4038238eb0fc3d2d
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-05-16 00:20:25 +0400

    Add files via upload

--- bars.py
+++ bars.py
@@ -1,80 +1,90 @@
 import requests
 import json
-import misc
-token = misc.token
-url = "https://apidata.mos.ru/v1/features/1796"
 
 
 def get_bars():
-    payload = {"api_key":token}
-    response_from_url = requests.get(url, params=payload)
-    r_json = response_from_url.json()
-    bars = r_json["features"]
+    url = "https://devman.org/media/filer_public/\
+95/74/957441dc-78df-4c99-83b2-e93dfd13c2fa/bars.json"
+    response_from_serv = requests.get(url)
+    response_file = response_from_serv.json()
+    bars = response_file["features"]
     return bars
 
 
-def get_min_max():
-
-    # создание списка для поиска max и min
-    arr_0 = []
+def get_max_size():
+    buffer_to_findmax = []
     for list_item in get_bars():
         seats = list_item["properties"]["Attributes"]["SeatsCount"]
-
-# добавляем в список
-        arr_0.append(seats)
-    max_size = max(arr_0)
-    min_size = min(arr_0)
+        buffer_to_findmax.append(seats)
+    max_size = max(buffer_to_findmax)
     print("Вместимость самого большого бара {}".format(max_size))
-    print("Вместимость самого маленького бара {}".format(min_size))
     for list_item in get_bars():
         seats = list_item["properties"]["Attributes"]["SeatsCount"]
         if max_size == seats:
             print("Самый большой бар: ")
             print(list_item["properties"]["Attributes"]["Name"])
-        elif min_size == seats:
+
+
+def get_min_size():
+    buffer_to_findmin = []
+    for list_item in get_bars():
+        seats = list_item["properties"]["Attributes"]["SeatsCount"]
+        buffer_to_findmin.append(seats)
+    min_size = min(buffer_to_findmin)
+    print("Вместимость самого маленького бара {}".format(min_size))
+    for list_item in get_bars():
+        seats = list_item["properties"]["Attributes"]["SeatsCount"]
+        if min_size == seats:
             print("Самый маленький бар: ")
             print(list_item["properties"]["Attributes"]["Name"])
 
 
-def find_bar(x_coord, y_coord):
-    arr_1 = []
+def find_nearest_bar(x_coord, y_coord):
+    coord_buff = []
     for list_item in get_bars():
         coordinates = list_item["geometry"]["coordinates"]
-        arr_1.append(coordinates)
-    arr_2 = []
-    for list_item in arr_1:
-
+        coord_buff.append(coordinates)
+    nearest_bar_buff = []
+    for list_item in coord_buff:
         diff_x = x_coord-list_item[0]
-
-# находим разницу по координатам
-
         diff_y = y_coord-list_item[1]
-
-# находим сумму разности x и y
         sum_of_diffs = abs(diff_x) + abs(diff_y)
-
-        arr_2.append(sum_of_diffs)
-
-# находим минимальное отклонение от координат
-        min_coord = min(arr_2)
-
-# находим индекс
-    min_index = arr_2.index(min_coord)
+        nearest_bar_buff.append(sum_of_diffs)
+        min_coord = min(nearest_bar_buff)
+    min_index = nearest_bar_buff.index(min_coord)
 
     return get_bars()[min_index]["properties"]["Attributes"]["Name"]
 
 
-def main():
-    get_min_max()
+def get_input_x():
     x_coord = input("Введите координату х \n")
-    y_coord = input("Введите координату y \n")
     try:
         float(x_coord)
+    except ValueError:
+        print("""Неверный формат координат, координаты должны быть
+заданы числом с плавающей точкой""")
+    return x_coord
+
+
+def get_input_y():
+    y_coord = input("Введите координату х \n")
+    try:
         float(y_coord)
     except ValueError:
-        print("Неверный формат координат")
-        main()
-    print("Ближайший к Вам бар: ", find_bar(float(x_coord), float(y_coord)))
+        print("""Неверный формат координат, координаты должны быть
+заданы числом с плавающей точкой""")
+    return y_coord
+
+
+def bars_print():
+    get_min_size()
+    get_max_size()
+    print("Ближайший к Вам бар: ",
+          find_nearest_bar(float(get_input_x()), float(get_input_x())))
+
+
+def main():
+    bars_print()
 
 
 if __name__ == '__main__':
