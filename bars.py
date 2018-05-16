0afe3c20bda5a2946915b9e14c656f56c2e0aeee
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-05-17 02:02:36 +0400

    упрощена функция нахождения ближайшего бара

--- bars.py
+++ bars.py
@@ -2,44 +2,40 @@ import json
 import sys
 
 
-def get_json(filepath):
+def get_info(filepath):
     with open(filepath, "r", encoding="utf-8") as file_handler:
         return json.load(file_handler)
 
 
-def get_bars():
-    data_file = get_json(filepath)
-    bars = data_file["features"]
+def get_bars(bars_info):
+    bars = bars_info["features"]
     return bars
 
 
-def max_bar_name():
-    bars = get_bars()
+def get_max_bar_name(bars):
     max_bar = max(bars, key=lambda bar: bar["properties"]["Attributes"]["SeatsCount"])
-    return max_bar["properties"]["Attributes"]["Name"], max_bar["properties"]["Attributes"]["SeatsCount"]
+    return max_bar["properties"]["Attributes"]["Name"]
 
 
-def min_bar_name():
-    bars = get_bars()
+def get_max_seats(bars):
+    max_seats = max(bars, key=lambda bar: bar["properties"]["Attributes"]["SeatsCount"])
+    return max_seats["properties"]["Attributes"]["SeatsCount"]
+
+
+def get_min_bar_name(bars):
     min_bar = min(bars, key=lambda bar: bar["properties"]["Attributes"]["SeatsCount"])
-    return min_bar["properties"]["Attributes"]["Name"], min_bar["properties"]["Attributes"]["SeatsCount"]
+    return min_bar["properties"]["Attributes"]["Name"]
+
+
+def get_min_seats(bars):
+    min_seats = min(bars, key=lambda bar: bar["properties"]["Attributes"]["SeatsCount"])
+    return min_seats["properties"]["Attributes"]["SeatsCount"]
 
 
 def find_nearest_bar(x_coord, y_coord):
-    coord_buff = []
-    data_file = get_bars()
-    for list_item in data_file:
-        coordinates = list_item["geometry"]["coordinates"]
-        coord_buff.append(coordinates)
-    nearest_bar_buff = []
-    for list_item in coord_buff:
-        diff_x = (x_coord-list_item[0])**2
-        diff_y = (y_coord-list_item[1])**2
-        sum_sqr_of_diffs = (diff_x + diff_y)**0.5
-        nearest_bar_buff.append(sum_sqr_of_diffs)
-        min_coord = min(nearest_bar_buff)
-    min_index = nearest_bar_buff.index(min_coord)
-    return get_bars()[min_index]["properties"]["Attributes"]["Name"]
+    nearest_bar = min(bars, key=lambda z: ((z["geometry"]["coordinates"][0]-x_coord)**2
+                                 +(z["geometry"]["coordinates"][1]-y_coord)**2)**0.5)
+    return (nearest_bar["properties"]["Attributes"]["Name"])
 
 
 def get_coord():
@@ -48,32 +44,31 @@ def get_coord():
     try:
         float(x_coord)
         float(y_coord)
-        return x_coord, y_coord
+        return float(x_coord), float(y_coord)
     except ValueError:
-        print("""Неверный формат координат, координаты должны быть
-    заданы числом с плавающей точкой""")
+        print("Неверный формат координат, координаты должны быть")
+        print("заданы числом с плавающей точкой")
 
 
-
-def print_bar():
-    min_bar, min_size_of_bar = min_bar_name()
-    max_bar, max_size_of_bar = max_bar_name()
-    print("Вместимость самого большого бара: ", max_size_of_bar)
-    print("Вместимость самого маленького бара: ", min_size_of_bar)
-    print("Самый большой бар: ", max_bar)
-    print("Самый маленький бар: ", min_bar)
-    x_coord, y_coord = get_coord()
+def print_bar(max_bar_name, min_bar_name, nearest_bar):
+    max_seats = get_max_seats(bars)
+    min_seats = get_min_seats(bars)
+    print("Вместимость самого большого бара: ", max_seats)
+    print("Вместимость самого маленького бара: ", min_seats)
+    print("Самый большой бар: ", max_bar_name)
+    print("Самый маленький бар: ", min_bar_name)
     print("Ближайший к Вам бар: ")
-    print(find_nearest_bar(float(x_coord), float(y_coord)))
+    print(nearest_bar)
 
-
-def main():
-    print_bar()
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        filepath = format(sys.argv[1])
-    else:
-        filepath = input("Укажите путь до json файла: ")
-    main()
+        filepath = sys.argv[1]
+    bars_info = get_info(filepath)
+    bars = get_bars(bars_info)
+    min_bar_name = get_min_bar_name(bars)
+    max_bar_name = get_max_bar_name(bars)
+    x_coord, y_coord = get_coord()
+    nearest_bar = find_nearest_bar(x_coord, y_coord)
+    print_bar(max_bar_name, min_bar_name, nearest_bar)
